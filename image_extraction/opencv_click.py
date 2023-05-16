35064d695190878ff1a012b0441b6570860c8de8
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-05-17 00:52:27 +0200

    Add transformation

--- image_extraction/opencv_click.py
+++ image_extraction/opencv_click.py
@@ -1,14 +1,10 @@
 import cv2
 from command_line_parser import parser
-from pynput import keyboard
-
-def handle_keyboard_event(key: keyboard.Key):
-    if key == keyboard.Key.esc:
-        print("ESC pressed")
+import numpy as np
 
 
 def init() -> None:
-    global INPUT_PATH, OUTPUT_PATH, WIDTH, HEIGHT, LISTENER, IMAGE
+    global INPUT_PATH, OUTPUT_PATH, WIDTH, HEIGHT, IMAGE, WINDOW_NAME, ORIG_IMAGE, MARKERLIST, transformed
 
     args = parser.parse_args()
 
@@ -19,32 +15,69 @@ def init() -> None:
 
     args.input.close()
 
-    LISTENER = keyboard.Listener(on_release=handle_keyboard_event)
     IMAGE = cv2.imread(INPUT_PATH)
+    ORIG_IMAGE = IMAGE.copy()
 
-def place_marker(x_pos: int, y_pos: int):
-    global IMAGE
+    WINDOW_NAME = 'Preview Window'
+
+    MARKERLIST = []
+
+    transformed = False
+
+    cv2.namedWindow(WINDOW_NAME)
+
+    cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
+
+
+def transform() -> None:
+    global transformed
+
+    points = np.float32(np.array(MARKERLIST))
+    print(f'Points: {points}')
+    destination = np.float32(
+        np.array([[0, 0], [WIDTH, 0], [WIDTH, HEIGHT], [0, HEIGHT]]))
+    matrix = cv2.getPerspectiveTransform(points, destination)
+    img_transformed = cv2.warpPerspective(
+        ORIG_IMAGE.copy(), matrix, (WIDTH, HEIGHT), flags=cv2.INTER_LINEAR)
+    cv2.imshow(WINDOW_NAME, cv2.flip(img_transformed, 0))
+    transformed = True
+
+
+def place_marker(x_pos: int, y_pos: int) -> None:
+    global IMAGE, MARKERLIST
+
     IMAGE = cv2.circle(IMAGE, (x_pos, y_pos), 5, (255, 0, 0), -1)
+    MARKERLIST.append([x_pos, y_pos])
     cv2.imshow(WINDOW_NAME, IMAGE)
+    if len(MARKERLIST) == 4:
+        transform()
 
-def reset_markers():
-    pass
 
-def mouse_callback(event: int, x: int, y: int, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        place_marker(x,y)
+def reset_canvas() -> None:
+    global IMAGE, MARKERLIST, transformed
+    MARKERLIST.clear()
+    IMAGE = ORIG_IMAGE.copy()
+    cv2.imshow(WINDOW_NAME, IMAGE)
+    transformed = False
+
+
+def mouse_callback(event: int, x: int, y: int, flags, param) -> None:
+    if event == cv2.EVENT_LBUTTONDOWN and not transformed:
+        place_marker(x, y)
 
 
 if __name__ == '__main__':
     init()
 
-    WINDOW_NAME = 'Preview Window'
-
-    cv2.namedWindow(WINDOW_NAME)
-
-    cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
     cv2.imshow(WINDOW_NAME, IMAGE)
 
-    # LISTENER.start()
+    # https://stackoverflow.com/questions/35003476/opencv-python-how-to-detect-if-a-window-is-closed/37881722#37881722
+    while cv2.getWindowProperty(WINDOW_NAME, 0) == 0:
+        button = cv2.waitKey(0)
 
-    cv2.waitKey(-1)
+        if button == 27:
+            reset_canvas()
+        if button == ord('s') and transformed:
+            print("Saving image...")
+        if button == ord('q'):
+            break
