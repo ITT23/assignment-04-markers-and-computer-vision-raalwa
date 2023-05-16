d653f4ffd95b53d2287f0c2cad4477c9fee303b5
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-05-16 23:18:45 +0200

    Decompose

--- image_extraction/opencv_click.py
+++ image_extraction/opencv_click.py
@@ -1,18 +1,50 @@
 import cv2
+from command_line_parser import parser
+from pynput import keyboard
 
-img = cv2.imread('sample_image.jpg')
-WINDOW_NAME = 'Preview Window'
+def handle_keyboard_event(key: keyboard.Key):
+    if key == keyboard.Key.esc:
+        print("ESC pressed")
 
-cv2.namedWindow(WINDOW_NAME)
 
-def mouse_callback(event, x, y, flags, param):
-    global img
+def init() -> None:
+    global INPUT_PATH, OUTPUT_PATH, WIDTH, HEIGHT, LISTENER, IMAGE
 
+    args = parser.parse_args()
+
+    INPUT_PATH = args.input.name
+    OUTPUT_PATH = args.output
+    WIDTH = args.width
+    HEIGHT = args.height
+
+    args.input.close()
+
+    LISTENER = keyboard.Listener(on_release=handle_keyboard_event)
+    IMAGE = cv2.imread(INPUT_PATH)
+
+def place_marker(x_pos: int, y_pos: int):
+    global IMAGE
+    IMAGE = cv2.circle(IMAGE, (x_pos, y_pos), 5, (255, 0, 0), -1)
+    cv2.imshow(WINDOW_NAME, IMAGE)
+
+def reset_markers():
+    pass
+
+def mouse_callback(event: int, x: int, y: int, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        img = cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
-        cv2.imshow(WINDOW_NAME, img)
+        place_marker(x,y)
 
-cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
-cv2.imshow(WINDOW_NAME, img)
 
-cv2.waitKey(0)
+if __name__ == '__main__':
+    init()
+
+    WINDOW_NAME = 'Preview Window'
+
+    cv2.namedWindow(WINDOW_NAME)
+
+    cv2.setMouseCallback(WINDOW_NAME, mouse_callback)
+    cv2.imshow(WINDOW_NAME, IMAGE)
+
+    # LISTENER.start()
+
+    cv2.waitKey(-1)
